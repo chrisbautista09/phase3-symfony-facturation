29d79a1658f6aace5b4f87695ae086ac06a5df05
chrisbautista09 <chrisbautista.cb@gmail.com>
2026-04-29 16:45:57 +0200

    creation auth register login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,35 +4,40 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $firt_name = null;
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
 
-    #[ORM\Column(length: 255)]
-    private ?string $last_name = null;
-
-    #[ORM\Column(length: 255)]
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $company_name = null;
+    private ?string $first_name = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $iban = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $siret = null;
+    private ?string $last_name = null;
 
     public function getId(): ?int
     {
@@ -51,30 +56,41 @@ class User
         return $this;
     }
 
-    public function getFirtName(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->firt_name;
+        return (string) $this->email;
     }
 
-    public function setFirtName(string $firt_name): static
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        $this->firt_name = $firt_name;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
 
         return $this;
     }
 
-    public function getLastName(): ?string
-    {
-        return $this->last_name;
-    }
-
-    public function setLastName(string $last_name): static
-    {
-        $this->last_name = $last_name;
-
-        return $this;
-    }
-
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -87,38 +103,37 @@ class User
         return $this;
     }
 
-    public function getCompanyName(): ?string
+    /**
+     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
+     */
+    public function __serialize(): array
     {
-        return $this->company_name;
+        $data = (array) $this;
+        $data["\0" . self::class . "\0password"] = hash('crc32c', $this->password);
+        
+        return $data;
     }
 
-    public function setCompanyName(string $company_name): static
+    public function getFirstName(): ?string
     {
-        $this->company_name = $company_name;
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): static
+    {
+        $this->first_name = $first_name;
 
         return $this;
     }
 
-    public function getIban(): ?string
+    public function getLastName(): ?string
     {
-        return $this->iban;
+        return $this->last_name;
     }
 
-    public function setIban(string $iban): static
+    public function setLastName(string $last_name): static
     {
-        $this->iban = $iban;
-
-        return $this;
-    }
-
-    public function getSiret(): ?string
-    {
-        return $this->siret;
-    }
-
-    public function setSiret(string $siret): static
-    {
-        $this->siret = $siret;
+        $this->last_name = $last_name;
 
         return $this;
     }
