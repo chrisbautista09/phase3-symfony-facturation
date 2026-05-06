b57a51e9cca9f90c93c353bb2c1d504db80752f3
chrisbautista09 <chrisbautista.cb@gmail.com>
2026-05-06 16:54:50 +0200

    gotenberg pdf

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Sensiolabs\GotenbergBundle\SensiolabsGotenbergBundle::class => ['all' => true],
 ];
--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -2,23 +2,38 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
+use Sensiolabs\GotenbergBundle\GotenbergPdfInterface;
+use Sensiolabs\GotenbergBundle\Processor\TempfileProcessor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\File;
 
 final class MailController extends AbstractController
 {
+    public function __construct(private LoggerInterface $logger){}
+
+
     #[Route('/mail', name: 'app_mail')]
-    public function index(MailerInterface $mailer): Response
+    public function index(MailerInterface $mailer, GotenbergPdfInterface $gotenberg): Response
     {
+        $filePdf = $gotenberg->html()
+        ->content($this->renderView('test/index.html.twig'))
+        ->processor(new TempfileProcessor())
+        ->generate()
+        ->process();
+
         $email = new Email();
         $email->from("trucmachin@laposte.com")
         ->to("bidulle.chouet@gladalle.com")
         ->subject("le peuple à faim")
-        ->text("alors qui mange");
-
+        ->html("Coucou")
+        ->attachFromPath($filePdf->getPathname(), 'document.pdf');
+        
         $mailer->send($email);
         
         return $this->render('mail/index.html.twig', [
