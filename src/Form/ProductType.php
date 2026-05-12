0fcbac133191cd18c0741f4d5e6eccf0d575bcb8
chrisbautista09 <chrisbautista.cb@gmail.com>
2026-05-12 16:52:48 +0200

    view new product end

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,6 +6,9 @@ use App\Entity\InvoiceItem;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,17 +17,19 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name',textType::class, [
-               'label' => 'nom',
+            ->add('name',TextType::class, [
+               'label' => 'Nom',
             ])
-            ->add('description')
-            ->add('price')
-            ->add('UnitEnum')
-            ->add('user', EntityType::class, [
-                'class' => InvoiceItem::class,
-                'choice_label' => 'id',
+            ->add('description',TextareaType::class, [
+                'label'=> 'Description',
             ])
-        ;
+            ->add('price', NumberType::class, [
+                'label'=> 'Prix',
+            ])
+            ->add('UnitEnum', null, [
+                'label'=> 'Unité',
+            ])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
